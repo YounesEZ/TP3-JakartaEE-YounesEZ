97c7b69347df471f59074c49b089cb616f41c84f
YounesEZ <ezbidayounes@gmail.com>
2024-12-14 13:05:28 +0100

    ajout de la class LlmClient avec role system

--- src/main/java/ma/emsi/tp3ezbidayounes/GuideTouristiquResource.java
+++ src/main/java/ma/emsi/tp3ezbidayounes/GuideTouristiquResource.java
@@ -5,6 +5,7 @@ import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.Response;
 
 @Path("/guide")
 public class GuideTouristiquResource {
@@ -14,9 +15,9 @@ public class GuideTouristiquResource {
 
     @GET
     @Path("lieu/{ville_ou_pays}")
-    @Produces("text/plain")
-    public String visit(@PathParam("ville_ou_pays") String ville_ou_pays) {
-        return ville_ou_pays;
+    @Produces("application/json")
+    public Response visit(@PathParam("ville_ou_pays") String ville_ou_pays) {
+        return Response.ok(llmClient.envoyerMessage("Je veux visiter" + ville_ou_pays)).build();
     }
 
 
